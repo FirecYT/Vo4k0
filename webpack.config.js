a18e28ca607df8e5b2d7889573f030d1a585501d
Firec <firecft@yandex.ru>
2022-08-17 16:20:59 +0300

    Система блоков в webpack Пародия на архитектуру

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	entry: './src/index.js',
 	devtool: 'inline-source-map',
 	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	plugins: [new MiniCssExtractPlugin({
+		filename: "style.css"
+	})],
 	module: {
 		rules: [
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
-					"style-loader",
+					MiniCssExtractPlugin.loader,
 					"css-loader",
 					"sass-loader",
 				]
